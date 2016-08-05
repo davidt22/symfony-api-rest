8813d6f45d7e7be8c3bfc5743575c1080a710611
David Teruel <d_teruel@hotmail.com>
2016-08-05 14:48:48 +0200

    Cap-22: Created service to parse response in Json format

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,14 +2,16 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @param Request $request
+     *
+     * @return Response
      */
     public function indexAction(Request $request)
     {
@@ -18,4 +20,19 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
     }
+
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function pruebasAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository('BackendBundle:User')->findAll();
+        $helpersService = $this->get('app.helpers');
+
+        return $helpersService->parseJson($users);
+    }
+
 }
