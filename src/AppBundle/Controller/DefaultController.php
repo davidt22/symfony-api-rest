483a094c8c0e2bd9dbcec97b5d38d68ca364de77
David Teruel <d_teruel@hotmail.com>
2016-08-08 12:31:06 +0200

    Cap 24: save changes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints\Email;
 
 class DefaultController extends Controller
 {
@@ -33,6 +34,34 @@ class DefaultController extends Controller
         $helpersService = $this->get('app.helpers');
 
         return $helpersService->parseJson($users);
+    }
+
+    public function loginAction(Request $request)
+    {
+        $helpersService = $this->get('app.helpers');
+        $json = $request->get('json');
+
+        if($json){
+            $params = json_decode($json);
+
+            $email = $params->email ? $params->email : null;
+            $password = $params->password ? $params->password : null;
+
+            $emailContraint = new Email();
+            $emailContraint->message = 'Email not valid';
+
+            $validateEmail = $this->get('validator')->validate($email, $emailContraint);
+
+            if(count($validateEmail) == 0 && $password != null){
+                echo 'Data Success';
+            }else{
+                echo 'Data incorrect';
+            }
+        }else{
+            echo 'Send JSON with POST';
+                die();
+        }
+
     }
 
 }
