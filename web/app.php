667dee159ccbda69dca5658a9ef33e54b0f98fc1
David Teruel <d_teruel@hotmail.com>
2016-08-16 11:44:18 +0200

    Cap 47: headers configuration

--- web/app.php
+++ web/app.php
@@ -2,6 +2,15 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+header('Access-Control-Allow-Origin: *');//desde cualquier dominio se puedan hacer peticiones
+header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Request-Method');//los tipos de cabeceras permitidos
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');//los metodos disponibles (OPTIONS para ficheros en AngularJS)
+header('Allow: GET, POST, OPTIONS, PUT, DELETE');
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == 'OPTIONS'){ //en angular da problemas este metodo y entonces tiene que enviarse por POST
+    die();
+}
+
 /**
  * @var Composer\Autoload\ClassLoader
  */
--- web/app_dev.php
+++ web/app_dev.php
@@ -10,12 +10,21 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+//if (isset($_SERVER['HTTP_CLIENT_IP'])
+//    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+//    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
+//) {
+//    header('HTTP/1.0 403 Forbidden');
+//    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+//}
+
+header('Access-Control-Allow-Origin: *');//desde cualquier dominio se puedan hacer peticiones
+header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Request-Method');//los tipos de cabeceras permitidos
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');//los metodos disponibles (OPTIONS para ficheros en AngularJS)
+header('Allow: GET, POST, OPTIONS, PUT, DELETE');
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == 'OPTIONS'){ //en angular da problemas este metodo y entonces tiene que enviarse por POST
+    die();
 }
 
 /**
